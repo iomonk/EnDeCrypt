ee741218481593daac978086bcee5f25effc1ea5
iomonk <59078897+iomonk@users.noreply.github.com>
2021-04-20 17:34:23 -0600

    Separate Services (#2)

---
+++ EnDeCrypt/Services/DecryptService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace EnDeCrypt.Services
+{
+    public static class DecryptService
+    {
+        public static string DecryptStringFromBytes(byte[] cipherText, byte[] key, byte[] iv)
+        {
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException(nameof(cipherText));
+            if (key == null || key.Length <= 0)
+                throw new ArgumentNullException(nameof(key));
+            if (iv == null || iv.Length <= 0)
+                throw new ArgumentNullException(nameof(iv));
+
+            using var aesAlg = new AesManaged {Key = key, IV = iv};
+
+            var decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+            using var msDecrypt = new MemoryStream(cipherText);
+            using var csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read);
+            using var srDecrypt = new StreamReader(csDecrypt);
+            var plaintext = srDecrypt.ReadToEnd();
+
+            return plaintext;
+        }
+    }
+}
---
+++ EnDeCrypt/Services/EncryptService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace EnDeCrypt.Services
+{
+    public static class EncryptService
+    {
+        public static byte[] EncryptStringToBytes(string plainText, byte[] key, byte[] iv)
+        {
+            if (plainText == null || plainText.Length <= 0)
+                throw new ArgumentNullException(nameof(plainText));
+            if (key == null || key.Length <= 0)
+                throw new ArgumentNullException(nameof(key));
+            if (iv == null || iv.Length <= 0)
+                throw new ArgumentNullException(nameof(iv));
+
+            using var aesAlg = new AesManaged {Key = key, IV = iv};
+
+            var encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            using var msEncrypt = new MemoryStream();
+            using var csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
+            using (var swEncrypt = new StreamWriter(csEncrypt))
+            {
+                swEncrypt.Write(plainText);
+            }
+
+            var encrypted = msEncrypt.ToArray();
+
+            return encrypted;
+        }
+    }
+}
